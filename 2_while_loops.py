925504bfef2a6d79b8f7db3e5bab482d77b05f6d
rdabelo <rodeloabelo@gmail.com>
2025-12-09 15:25:22 +0000

    while loops

--- 2_while_loops.py
+++ 2_while_loops.py
@@ -1,4 +1,42 @@
 # Given:
+# while loops = execute some code while some condition remain true
+# name  = input( "enter your name: ")
+
+# if name == "":
+#     print(" you did not enter your name")
+#     name=input("enter your name: ")
+# else:
+#     print(f"Hello {name}")
+
+# while name == "":
+# age = int(input("Enter your age: "))
+
+# while age < 0:
+#     print("age cam't be negative")
+#     age = int(input("enter your age: "))
+
+# print(f" You are {age} years old")\
+
+# food = input("Enter a food you like (q to quit): ")
+
+# favorite_foodlist = []
+
+# while not food == "quit":
+#     print(f"you like {food}")
+#     food = input("enter another food youy would like (q to quit): ")
+    
+# print ("bye")
+
+# favorite_foodlist.append(food)
+
+num = int(input(" Enter a # between 1 - 10: "))
+
+while num < 1 or num > 10:
+    print(f"{num} is not valid")
+    num = int(input("Enter a # between 1 - 10: "))
+
+print(f"your number is {num}")
+
 colors = ["red", "blue", "green", "yellow", "purple"]
 
 # Challenge:
